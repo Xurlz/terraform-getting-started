c29a4340f7893c4fd14209d02c02f7d4d9737141
Charles Sena <charlessena@gokeitecnologia.com.br>
2026-05-11 16:45:44 -0300

    chore: Implement deploy specs

--- main.tf
+++ main.tf
@@ -8,3 +8,23 @@ terraform {
   required_version = ">=1.2"
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name = "name"
+    values= ["ubuntu/images/hvm-ssd-gp3/ubuntu-resolute-26.04-amd64-server-*"]
+  }
+
+  owners=["099720109477"] # Canonical Id
+}
+
+resource "aws_instance" "app_server" {
+  ami           = aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags {
+    Name = "learn-terraform"
+  }
+}
+
