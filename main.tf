aeeb916109dcc71dc9336955e4c09aaae3d92e07
Charles Sena <charlessena@gokeitecnologia.com.br>
2026-05-13 15:30:19 -0300

    chore: Add keyname

--- main.tf
+++ main.tf
@@ -26,6 +26,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
+  key_name      = "treinamento-ci-cd-api-go"
 
   tags = {
     Name = "learn-terraform"
